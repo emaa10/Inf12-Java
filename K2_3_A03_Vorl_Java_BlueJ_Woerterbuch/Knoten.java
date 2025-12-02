616bd7ffb9c5382b3c6848c65c863cbf798835e0
Emanuel Berger <emanuelberger49@gmail.com>
2025-12-02 12:21:14 +0100

    update

--- K2_3_A03_Vorl_Java_BlueJ_Woerterbuch/Knoten.java
+++ K2_3_A03_Vorl_Java_BlueJ_Woerterbuch/Knoten.java
@@ -1,10 +1,12 @@
 
-public class Knoten
+public class Knoten extends Baumelement
 {
     public Baumelement nachfolgerLinks;
     public Baumelement nachfolgerRechts;
     public Datenelement daten;
+    
     public Knoten(Datenelement dNeu) {
+        super();
         daten = dNeu;
         nachfolgerLinks = new Abschluss();
         nachfolgerRechts = new Abschluss();
