ecc5771606df8ec3d03a63dace57f20311e9caad
Emanuel Berger <emanuelberger49@gmail.com>
2026-01-16 11:53:07 +0100

    add dreieck

---
+++ K3_1_A11_Vorl_Java_BlueJ_Sierpinski_Dreieck/SierpinskiDreieck.java
@@ -0,0 +1,48 @@
+
+/**
+ * Erzeugt das Sierpinski-Dreieck.
+ * 
+ * @author Albert Wiedemann
+ * @version 1.0
+ */
+class SierpinskiDreieck
+{
+
+    /**
+     * Legt das Grunddreieck an und stößt die Rekursion an
+     * @@param tiefe Rekursionstiefe
+     */
+    SierpinskiDreieck (int tiefe)
+    {
+        Dreieck basis = new Dreieck();
+        basis.FarbeSetzen("weiß");
+        basis.PositionSetzen(400, 10);
+        basis.GrößeSetzen(600, 520);
+        SchrittAusführen(400, 10, 600, 520, tiefe);
+    }
+    
+    /**
+     * Ermittelt den Rekursionsschritt
+     * @x x-Position der Dreiecksspitze
+     * @y y-Position der Dreiecksspitze
+     * @breite die Breite des Umgebungsdreiecks
+     * @höhe die Höhe des Umgebungsdreiecks
+     * @tiefe restliche Rekursionstiefe
+     */
+    void SchrittAusführen(int x, int y, int breite, int höhe, int tiefe)
+    {
+        if (tiefe > 0)
+        {
+            SchrittAusführen(x, y, breite / 2, höhe / 2, tiefe - 1); // oben
+            SchrittAusführen(x, y, breite / 2, höhe / 2, tiefe - 1); // links unten
+            SchrittAusführen(x, y, breite / 2, höhe / 2, tiefe - 1); // rechts
+        }
+        else
+        {
+            Dreieck basis = new Dreieck();
+            basis.FarbeSetzen("schwarz");
+            basis.PositionSetzen(x, y);
+            basis.GrößeSetzen(breite, höhe);
+        }
+    }
+}
